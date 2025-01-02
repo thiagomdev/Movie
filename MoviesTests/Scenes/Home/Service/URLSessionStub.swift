0f4436be44197ab70393467b961fc00976717d5b
Thiago Monteiro <thiagomonteiro.developer@gmail.com>
2025-01-01 22:57:52 -0300

    Added: - Create own URLSessionStub's file

---
+++ MoviesTests/Scenes/Home/Service/URLSessionStub.swift
@@ -0,0 +1,60 @@
+import Foundation
+
+final class URLSessionStub: URLProtocol {
+    private static var stub: Stub?
+    private static var observerRequest: ((URLRequest) -> Void)?
+    
+    private struct Stub {
+        let data: Data?
+        let response: URLResponse?
+        let error: Error?
+    }
+    
+    static func stub(data: Data?, response: URLResponse?, error: Error?) {
+        stub = Stub(data: data, response: response, error: error)
+    }
+    
+    static func observerRequests(_ observer: @escaping (URLRequest) -> Void) {
+        observerRequest = observer
+    }
+    
+    static func startInterceptionRequests() {
+        URLProtocol.registerClass(URLSessionStub.self)
+    }
+    
+    static func stopInterceptionRequests() {
+        URLProtocol.unregisterClass(URLSessionStub.self)
+        stub = nil
+        observerRequest = nil
+    }
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        if let observer = URLSessionStub.observerRequest {
+            client?.urlProtocolDidFinishLoading(self)
+            return observer(request)
+        }
+        
+        if let data = URLSessionStub.stub?.data {
+            client?.urlProtocol(self, didLoad: data)
+        }
+        
+        if let response = URLSessionStub.stub?.response {
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+        }
+        
+        if let error = URLSessionStub.stub?.error {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+        client?.urlProtocolDidFinishLoading(self)
+    }
+    
+    override func stopLoading() {}
+}
